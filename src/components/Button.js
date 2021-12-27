f8158e64eae177b5ef6594ab234aa10fd08c2d8b
Axolotlpi <cfc3.1415@gmail.com>
2021-12-27 14:14:09 -0600

    cleanup

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,9 +1,5 @@
 
-export default function Button(props) {
-    const {
-        text,
-        click
-    } = props;
+export default function Button({text, click}) {
 
     return (
         <button className=" text-semibold min-w-max w-full h-max p-2 rounded-full active:shadow-[inset_1px_1px_0px_2px_rgba(0,0,0,0.32)] bg-secondary-1 text-primary-0 transition-all duration-75"
--- src/components/Card/SummaryCard.js
+++ src/components/Card/SummaryCard.js
@@ -1,22 +1,19 @@
 
-function SummaryCard(props) {
-
-    const {
-        img,
-        heading,
-        text,
-    } = props
+function SummaryCard({img, heading, text}) {
 
     return (
     <div className="max-w-max max-h-max m-4 p-2 space-x-2 flex bg-primary-1 rounded-md shadow-lg">
-        <img src={img}
-            className="w-20 rounded-xl" />
-            <div className="flex flex-col">
-                <h3 className="font-san text-primary-3 font-bold"
-                >{heading}</h3>
-                <p className="font-sans text-primary-3"
-                >{text}</p> 
-            </div>
+        {
+            img &&
+            <img src={img}
+                className="w-20 rounded-xl" />
+        }
+        <div className="flex flex-col">
+            <h3 className="font-san text-primary-3 font-bold"
+            >{heading}</h3>
+            <p className="font-sans text-primary-3"
+            >{text}</p> 
+        </div>
     </div>
     );
   }
