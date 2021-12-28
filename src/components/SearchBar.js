6fe5320ca00d5be08c27930500b40515328b884d
Axolotlpi <cfc3.1415@gmail.com>
2021-12-27 19:52:10 -0600

    implement searchbar as form

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,11 +1,6 @@
 import { useState } from 'react';
 
-function SearchBar(props) {
-
-    const {
-        searchQuery,
-        placeholder
-    } = props;
+function SearchBar({searchQuery, placeholder}) {
 
     const [query, setQuery] = useState("");
 
@@ -13,15 +8,23 @@ function SearchBar(props) {
             setQuery(event.target.value);  
         }
 
+    const onSubmit = (event) => {
+            event.preventDefault();
+            searchQuery(query);
+        }
+
     return (
-    <div className="SearchBar w-full h-max flex flex-row justify-items-stretch font-sans font-semibold">
+    <form onSubmit={onSubmit}  
+        className="SearchBar w-full h-max flex flex-row justify-items-stretch font-sans font-semibold">
+
         <input type="text" placeholder={placeholder} onChange={handleChange} value={query} 
             className="bg-primary-0 placeholder:text-opacity-50 text-secondary-0 font-semibold p-2 rounded-l-md flex-[7]"/>
-        <button onClick={()=> searchQuery(query)} 
-            className="bg-primary-2 text-primary-0 font-bold p-2 rounded-r-md active:shadow-[inset_1px_1px_0px_2px_rgba(0,0,0,0.32)]">
+        
+        <button className="bg-primary-2 text-primary-0 font-bold p-2 rounded-r-md active:shadow-[inset_1px_1px_0px_2px_rgba(0,0,0,0.32)]">
             Search
         </button>
-    </div>
+
+    </form>
     );
 }
 
