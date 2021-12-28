4894ea14ebe0a753491f33677a9c87967afdf40e
Axolotlpi <cfc3.1415@gmail.com>
2021-12-27 21:08:11 -0600

    add auto erase to search submit

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,8 +1,8 @@
 import { useState } from 'react';
 
-function SearchBar({searchQuery, placeholder}) {
+function SearchBar({searchQuery, placeholder, autoCompleteArray}) {
 
-    const [query, setQuery] = useState("");
+    const [query, setQuery] = useState('');
 
     const handleChange = (event) => {
             setQuery(event.target.value);  
@@ -11,13 +11,18 @@ function SearchBar({searchQuery, placeholder}) {
     const onSubmit = (event) => {
             event.preventDefault();
             searchQuery(query);
+            setQuery('');
         }
+    
+    const autoComplete = () => {
+        if(!autoCompleteArray) return;
+    }
 
     return (
-    <form onSubmit={onSubmit}  
+    <form onSubmit={onSubmit} autoComplete="off"  
         className="SearchBar w-full h-max flex flex-row justify-items-stretch font-sans font-semibold">
 
-        <input type="text" placeholder={placeholder} onChange={handleChange} value={query} 
+        <input type="text" placeholder={placeholder} onChange={handleChange} value={query} onKeyUp={autoCompleteArray && autoComplete}
             className="bg-primary-0 placeholder:text-opacity-50 text-secondary-0 font-semibold p-2 rounded-l-md flex-[7]"/>
         
         <button className="bg-primary-2 text-primary-0 font-bold p-2 rounded-r-md active:shadow-[inset_1px_1px_0px_2px_rgba(0,0,0,0.32)]">
