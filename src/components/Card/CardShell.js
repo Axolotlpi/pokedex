62257236ed940d3881268d817ddad6e9a43876f2
Axolotlpi <cfc3.1415@gmail.com>
2021-12-27 19:52:24 -0600

    minor restyle

--- src/components/Card/CardShell.js
+++ src/components/Card/CardShell.js
@@ -1,7 +1,7 @@
 
 export default function CardShell(props){
     return (
-        <div className="bg-primary-1 rounded-3xl min-w-min h-full flex flex-col justify-center items-center p-2">
+        <div className="bg-primary-1 rounded-3xl min-w-min h-full flex flex-col justify-center items-center px-2 py-4">
             {props.children}
         </div>
     );
