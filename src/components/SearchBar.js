15058ce2207ca48aa54001c763ce10f1b0b14700
Axolotlpi <cfc3.1415@gmail.com>
2022-02-20 17:24:34 -0600

    add autocomplete hints to searchbar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,6 +2,7 @@ import { useState } from 'react';
 
 function SearchBar({ searchQuery, placeholder, autoCompleteArray }) {
   const [query, setQuery] = useState('');
+  const [suggestion, setSuggestion] = useState('');
 
   const handleChange = (event) => {
     setQuery(event.target.value);
@@ -14,7 +15,10 @@ function SearchBar({ searchQuery, placeholder, autoCompleteArray }) {
   };
 
   const autoComplete = () => {
-    if (!autoCompleteArray) return;
+    if (!autoCompleteArray || !query) return setSuggestion('');
+    const suggest = autoCompleteArray.find((word) => word.startsWith(query));
+    console.log(suggest);
+    setSuggestion(suggest ? suggest : '');
   };
 
   return (
@@ -23,14 +27,24 @@ function SearchBar({ searchQuery, placeholder, autoCompleteArray }) {
       autoComplete="off"
       className="SearchBar w-full h-max flex flex-row justify-items-stretch font-sans font-semibold"
     >
-      <input
-        type="text"
-        placeholder={placeholder}
-        onChange={handleChange}
-        value={query}
-        onKeyUp={autoCompleteArray && autoComplete}
-        className="bg-primary-0 placeholder:text-opacity-50 text-secondary-0 font-semibold p-2 rounded-l-md flex-[7]"
-      />
+      <div class="relative flex-[7] bg-primary-0 flex rounded-l-md">
+        <input
+          type="text"
+          placeholder={placeholder}
+          onChange={handleChange}
+          value={query}
+          onKeyUp={autoComplete}
+          className="w-full bg-transparent z-10 placeholder:text-opacity-50 text-secondary-0 font-semibold p-2 rounded-l-md"
+        />
+
+        <input
+          type="text"
+          value={suggestion}
+          readOnly
+          disabled
+          className="absolute w-full bg-transparent pointer-events-none text-opacity-50 text-secondary-0 font-semibold p-2 rounded-l-md"
+        />
+      </div>
 
       <button className="bg-primary-2 text-primary-0 font-bold p-2 rounded-r-md active:shadow-[inset_1px_1px_0px_2px_rgba(0,0,0,0.32)]">
         Search
